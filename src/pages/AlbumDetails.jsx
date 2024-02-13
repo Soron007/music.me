b422cd3f8a6ef9a30dc2f2e0cca395f76e7a2eb5
Soron007 <mitrasouvik123@gmail.com>
2024-02-13 23:37:51 +0530

    fetched album details

--- src/pages/AlbumDetails.jsx
+++ src/pages/AlbumDetails.jsx
@@ -1,9 +1,25 @@
-
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
 
 const AlbumDetails = () => {
+
+    const { id } = useParams();
+
+
+    const getAlbumDetails = async () => {
+        const response = await axios.get(`https://saavn.dev/albums?id=${id}`)
+        const { data } = await response.data;
+        console.log(data);
+    }
+
+    useEffect(() => {
+        getAlbumDetails();
+    }, [id])
+
     return (
         <div>
-            Album Details
+            {id}
         </div>
     )
 }
