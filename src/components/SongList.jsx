b78c0b79c5de1996e415f6f67557b480a03a881d
Soron007 <mitrasouvik123@gmail.com>
2024-02-22 01:15:31 +0530

    added playmusic functionality

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,4 +1,6 @@
 import { GoPlay } from 'react-icons/go'
+import { useContext } from 'react';
+import MusicContext from '../context/MainContext';
 
 const SongList = ({ name, primaryArtists, duration, downloadUrl, image, id }) => {
 
@@ -13,12 +15,14 @@ const SongList = ({ name, primaryArtists, duration, downloadUrl, image, id }) =>
         return `${formattedMinutes}: ${formattedSeconds}`
     }
 
+    const { isPlaying, currentSong, playMusic } = useContext(MusicContext)
+
     return (
         <div className='flex justify-between items-center w-[80vw] lg:w-[40vw] mb-2 lg:mb-1 p-1 px-3 hover:bg-white hover:shadow-md cursor-pointer'>
-            <GoPlay className='text-3xl text-gray-500 hover:text-gray-700 transition-all ease-in-out duration-300 cursor pointer' />
+            <GoPlay className='text-3xl text-gray-500 hover:text-gray-700 transition-all ease-in-out duration-300 cursor pointer' onClick={() => playMusic(downloadUrl, name, duration, image, id, primaryArtists)} />
 
             <div className='flex flex-col'>
-                <span className='text-center font-bold'>{name.length > 20 ? name.slice(0, 20) + "..." : name}</span>
+                <span className={`${id === currentSong?.id && "text-green-700"} text-center font-bold`}>{name.length > 20 ? name.slice(0, 20) + "..." : name}</span>
                 <span className='text-sm text-center font-extralight'>{primaryArtists.length > 20 ? primaryArtists.slice(0, 20) + "..." : primaryArtists}</span>
             </div>
 
